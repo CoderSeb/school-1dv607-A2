67c8b4a1e6acaa4f6be0181a8ce1e19892aee1dd
Sebastian <sa224ny@student.lnu.se>
2021-09-22 11:22:36 +0200

    Implemented Register model

--- src/main/java/model/Register.java
+++ src/main/java/model/Register.java
@@ -1,2 +1,44 @@
-package model;public class Register {
+package model;
+
+import java.util.ArrayList;
+
+public class Register {
+  private ArrayList<Member> members;
+
+  public Register() {
+    this.members = new ArrayList<Member>();
+  }
+
+  public ArrayList<Member> getMembers() {
+    return members;
+  }
+
+  public void setMembers(ArrayList<Member> members) {
+    this.members = members;
+  }
+
+  public void addMember(Member newMember) {
+    this.members.add(newMember);
+  }
+
+  public void removeMember(Member member) {
+    this.members.remove(member);
+  }
+
+  public ArrayList<String> getAllMemberIds() {
+    ArrayList<String> result = new ArrayList<String>();
+    for (Member member : members) {
+      result.add(member.getMemberId());
+    }
+    return result;
+  }
+
+  public Member findById(String id) {
+    for (Member member : this.members) {
+      if (member.getMemberId().equals(id)) {
+        return member;
+      }
+    }
+    return null;
+  }
 }
