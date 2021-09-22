331b867cdebb1669b0bde4f246f1fe2de0244087
Sebastian <sa224ny@student.lnu.se>
2021-09-22 11:31:02 +0200

    Added SampleTest.java and removed SimpleTest.java

---
+++ src/test/java/model/SampleTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+  @Test
+  public void firstSample() {
+
+    assertTrue(true);
+  }
+}
