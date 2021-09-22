e440955b48d00db098a387bee02f11ca47b1a824
Sebastian <sa224ny@student.lnu.se>
2021-09-22 11:23:23 +0200

    Implemented RegisterController controller

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -1,2 +1,39 @@
-package controller;public class RegisterController {
+package controller;
+
+import model.Boat;
+import model.Member;
+import model.Register;
+import view.RegisterView;
+
+public class RegisterController {
+  private RegisterView view;
+  private Register model;
+
+  public RegisterController(RegisterView view, Register model) {
+    this.view = view;
+    this.model = model;
+  }
+
+  public void validatePersonalNr() {
+    // TODO: implement validation...
+  }
+
+  public void createAndAddMember() {
+    String firstName = view.askFirstName();
+    String lastName = view.askLastName();
+    Long personalNr = view.askPersonalNr();
+    view.consumeLeftOverCharacters();
+    Member newMember = new Member(firstName, lastName, personalNr, model.getAllMemberIds());
+    model.addMember(newMember);
+  }
+
+  public void showMembersVerbose() {
+    for (Member member : model.getMembers()) {
+      view.printMemberVerbose(member.getFirstName(), member.getLastName(), member.getPersonalNr(), member.getMemberId());
+      view.printBoatSection();
+      for (Boat boat : member.getBoatList()) {
+        view.printBoat(boat.getName(), boat.getType(), boat.getLength());
+      }
+    }
+  }
 }
