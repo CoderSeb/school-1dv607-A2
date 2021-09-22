ca02bde69a4834504416f4b349004982a77e70fa
Sebastian <sa224ny@student.lnu.se>
2021-09-22 11:24:06 +0200

    Implemented ConsoleUi view class

--- src/main/java/view/ConsoleUi.java
+++ src/main/java/view/ConsoleUi.java
@@ -1,2 +1,32 @@
-package view;public class ConsoleUi {
+package view;
+
+import java.util.Scanner;
+
+public class ConsoleUi {
+  private Scanner scan;
+
+  public ConsoleUi() {
+    this.scan = new Scanner(System.in);
+  }
+  public Integer printMainOption() {
+    System.out.println("Please choose an option: ");
+    System.out.println("1 - Add new member.");
+    System.out.println("2 - Show verbose list of members.");
+    System.out.println("3 - Show compact list of members.");
+    System.out.println("0 - Quit.");
+    // TODO: Add more options.
+    return scan.nextInt();
+  }
+
+  public void printWelcomeMessage() {
+    System.out.println("Welcome to Jolly Roger yacht club!");
+  }
+
+  public void consumeLeftOvers() {
+    scan.nextLine();
+  }
+
+  public void closeScanner() {
+    scan.close();
+  }
 }
