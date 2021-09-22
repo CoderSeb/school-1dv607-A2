427ed3234f39b0ca2de8057ed6174a490fb29426
Sebastian <sa224ny@student.lnu.se>
2021-09-22 11:25:28 +0200

    Implemented start up interface in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,11 @@
-package controller;
+import controller.RegisterController;
+import model.BoatType;
+import model.Register;
+import view.ConsoleUi;
+import view.RegisterView;
 
 /**
- * Responsible for staring the application.
+ * Responsible for starting the application.
  */
 public class App {
   /**
@@ -10,6 +14,33 @@ public class App {
    * @param args command line arguments.
    */
   public static void main(String[] args) {
-    System.out.println("Let's begin!");
+    Boolean quit = false;
+    ConsoleUi mainUi = new ConsoleUi();
+    Register regModel = new Register();
+    RegisterView regView = new RegisterView();
+    RegisterController regController = new RegisterController(regView, regModel);
+
+    mainUi.printWelcomeMessage();
+    do {
+      switch(mainUi.printMainOption()) {
+        case(1):
+          mainUi.consumeLeftOvers();
+          regController.createAndAddMember();
+          break;
+        case(2):
+          mainUi.consumeLeftOvers();
+          regController.showMembersVerbose();
+          break;
+        case(3):
+          // TODO: show members compact.
+          break;
+        default:
+          mainUi.closeScanner();
+          quit = true;
+          break;
+      }
+
+    } while (!quit);
+
   }
 }
