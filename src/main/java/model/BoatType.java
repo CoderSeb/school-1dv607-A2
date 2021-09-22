5cade24f84565db9d8fabd462e907eb5ad652586
Sebastian <sa224ny@student.lnu.se>
2021-09-22 11:20:30 +0200

    Implemented BoatType enum

--- src/main/java/model/BoatType.java
+++ src/main/java/model/BoatType.java
@@ -1,2 +1,14 @@
-package model;public enum BoatType {
+package model;
+
+public enum BoatType {
+  SAILBOAT("Sailboat"),
+  MOTORSAILER("Motorsailer"),
+  CANOEORKAYAK("Canoe/Kayak"),
+  OTHER("Other");
+
+  public final String label;
+
+  private BoatType(String label) {
+    this.label = label;
+  }
 }
